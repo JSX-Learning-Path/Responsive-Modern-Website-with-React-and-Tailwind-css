8866ed9fc5f22df49fe14b3f24d0c7d85852b107
donni88c <cs_neeko@yahoo.com>
2026-01-21 10:38:07 +0200

    feat: enhance Features component layout with improved styling and text sections

--- modern-website/src/components/Features.jsx
+++ modern-website/src/components/Features.jsx
@@ -59,15 +59,21 @@ function Features() {
         </div>
         <div className="space-y-16 sm:space-y-32 lg:space-y-32">
           {features.map((feature, key) => (
-            <div key={key} className="">
+            <div
+              key={key}
+              className={`flex flex-col lg:flex-row items-center gap-8 sm:gap-12 ${feature.imagePosition === "right" ? "lg:flex-row-reverse" : "lg:flex-row"}`}
+            >
               {/* Images Section/Code Section */}
-              <div>
-                <div>
-                  {/* IDE */}
-                  <div>
-                    <div>
-                      <div>
-                        <div className="flex items-center space-x-2">
+              <div className="flex-1 w-full">
+                <div className="relative group">
+                  <div
+                    className=" relative bg-gray-900/50 backdrop-blur-sm border border-gray-700/50
+                rounded-xl sm:rounded-2xl p-4 sm:p-6 overflow-hidden group-hover:border-1 group-hover:border-blue-600/50 transition-all duration-300"
+                  >
+                    {/* IDE */}
+                    <div className="bg-gray-950 rounded-lg p-3 sm:p-4 font-mono text-xs sm:text-sm">
+                      <div className="flex items-center space-x-1 sm:space-x-2 mb-3 sm:mb-4 border-rounded">
+                        <div className="flex items-center space-x-2 sm:space-x-2">
                           <div className="w-2 h-2 sm:w-3 sm:h-3 rounded-full bg-red-500"></div>
                           <div className="w-2 h-2 sm:w-3 sm:h-3 rounded-full bg-yellow-500"></div>
                           <div className="w-2 h-2 sm:w-3 sm:h-3 rounded-full bg-green-500"></div>
@@ -81,20 +87,32 @@ function Features() {
                           language="javascript"
                           style={nightOwl}
                           customStyle={{
+                            backgroundColor: "transparent",
                             margin: 0,
                             textAlign: "left",
                             borderRadius: "8px",
-                            fontSize: "11px",
+                            fontSize: "12px",
                             height: "100%",
-                            lineHeight: "1.4",
-                            border: "1px solid #3c3c3c",
+                            lineHeight: "1.5",
                           }}
-                          >
+                          wrapLines={true}
+                        >
                           {feature.codeSnippet}
                         </SyntaxHighlighter>
                       </div>
                     </div>
                   </div>
+                </div>
+              </div>
+              {/*text section */}
+              <div className="flex-1 w-full">
+                <div className="mx-w-lg mx-auto lg:mx-0 text-center lg:text-left">
+                  <h2 className="text-4xl sm:text-3xl lg:text-4xl font-bold  mb-4 sm:mb-6 text-white">
+                    {feature.title}
+                  </h2>
+                  <p className="text-grey-300 text-base text-xl sm:text-lg leading-relaxed">
+                    {feature.description}
+                  </p>
                 </div>
               </div>
             </div>
